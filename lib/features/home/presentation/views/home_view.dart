cefb52f714e1029c48deee43fa9714cf5dd27c47
mohamed <mnafe44@gmail.com>
2026-05-29 11:58:34 +0300

    adding bottom navigation bar

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,10 +1,80 @@
+import 'dart:math' as math;
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  int currentIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: currentIndex == 0
+          ? const Center(child: Text('Home'))
+          : const Center(child: Text('Stats')),
+
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: BottomNavigationBar(
+          currentIndex: currentIndex,
+
+          onTap: (index) {
+            setState(() {
+              currentIndex = index;
+            });
+          },
+
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          backgroundColor: Colors.white,
+          elevation: 3,
+
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.graph_square_fill),
+              label: 'stats',
+            ),
+          ],
+        ),
+      ),
+
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+
+        shape: const CircleBorder(),
+
+        child: Container(
+          width: 60,
+          height: 60,
+
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Theme.of(context).colorScheme.tertiary,
+                Theme.of(context).colorScheme.secondary,
+                Theme.of(context).colorScheme.primary,
+              ],
+
+              transform: const GradientRotation(math.pi / 4),
+            ),
+
+            borderRadius: BorderRadius.circular(50),
+          ),
+
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
   }
 }
