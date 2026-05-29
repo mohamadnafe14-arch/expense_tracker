ac4c807a85c1f848e1062aaca318f20da4d57f3e
mohamed <mnafe44@gmail.com>
2026-05-29 11:25:15 +0300

    adding home view and app theme

---
+++ lib/core/widgets/app.dart
@@ -0,0 +1,24 @@
+import 'package:expense_tracker/features/home/presentation/views/home_view.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Expense Tracker',
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(
+          surface: Colors.grey.shade100,
+          onSurface: Colors.black,
+          primary: const Color(0xff00b2e7),
+          secondary: const Color(0xffe064f7),
+          tertiary: const Color(0xffff8d6c),
+        ),
+      ),
+      home: const HomeView(),
+    );
+  }
+}
