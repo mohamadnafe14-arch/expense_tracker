3da143be6e60626f5ffeb976055ab7b94a7b2d44
mohamed <mnafe44@gmail.com>
2026-06-02 12:03:40 +0300

    adding bloc observer

---
+++ lib/core/utils/app_bloc_observer.dart
@@ -0,0 +1,41 @@
+import 'dart:developer';
+
+import 'package:bloc/bloc.dart';
+
+class AppBlocObserver extends BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    log('${bloc.runtimeType} $change');
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    super.onError(bloc, error, stackTrace);
+    log('${bloc.runtimeType} $error $stackTrace');
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    log('${bloc.runtimeType} $transition');
+  }
+
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    super.onEvent(bloc, event);
+    log('${bloc.runtimeType} $event');
+  }
+
+  @override
+  void onCreate(BlocBase bloc) {
+    super.onCreate(bloc);
+    log('Created ${bloc.runtimeType}');
+  }
+
+  @override
+  void onClose(BlocBase bloc) {
+    super.onClose(bloc);
+    log('Closed ${bloc.runtimeType}');
+  }
+}
--- lib/core/utils/dependecy_injection.dart
+++ lib/core/utils/dependecy_injection.dart
@@ -1,3 +1,9 @@
+import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:expense_tracker/core/services/firestore_service.dart';
+import 'package:expense_tracker/core/utils/app_bloc_observer.dart';
+import 'package:expense_tracker/features/add_expense/data/repos/add_expense_repo.dart';
+import 'package:expense_tracker/features/add_expense/data/repos/add_expense_repo_imple.dart';
 import 'package:expense_tracker/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get_it/get_it.dart';
@@ -5,4 +11,12 @@ import 'package:get_it/get_it.dart';
 GetIt getIt = GetIt.instance;
 void setupLocator() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  Bloc.observer = AppBlocObserver();
+  getIt.registerSingleton<FirebaseFirestore>(FirebaseFirestore.instance);
+  getIt.registerSingleton<FirestoreService>(
+    FirestoreService(getIt<FirebaseFirestore>()),
+  );
+  getIt.registerSingleton<AddExpenseRepo>(
+    AddExpenseRepoImple(firestoreServices: getIt<FirestoreService>()),
+  );
 }
