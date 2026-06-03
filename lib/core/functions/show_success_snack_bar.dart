4653b5748cfa3235c95ec1ee41ca2f10bb159051
mohamed <mnafe44@gmail.com>
2026-06-03 12:06:51 +0300

    adding success snack bar

---
+++ lib/core/functions/show_success_snack_bar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:top_snackbar_flutter/top_snack_bar.dart';
+import 'package:top_snackbar_flutter/custom_snack_bar.dart';
+
+void showSuccessToast(BuildContext context, String message) {
+  showTopSnackBar(
+    Overlay.of(context),
+    CustomSnackBar.success(message: message),
+  );
+}
